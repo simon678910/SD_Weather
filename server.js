de69f954017169d3fc3cf63145c371ab86f9b299
simon <simon@localhost.localdomain>
2018-06-08 12:48:02 -0700

    Added express

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-var key = '6f0a0d476e913d7f62e149277ecc6289'
-var url = `http://api.openweathermap.org/data/2.5/forecast?id=sandiego,us&APPID=${key}`
+//setting up for the openweathermap
+const key = '6f0a0d476e913d7f62e149277ecc6289'
+var url = `http://api.openweathermap.org/data/2.5/forecast?q=SanDiego,us&APPID=${key}`
 
+//setting up express
+var express = require('express')
+var app = express()
+
+//setting up 
