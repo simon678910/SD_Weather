3e02a66fd27e0c4a4d1768ed2b8ef1656322f4fb
simon <simon@localhost.localdomain>
2018-06-08 13:44:30 -0700

    Changed the stucture,getting data from open weather

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 //setting up for the openweathermap
-const key = '6f0a0d476e913d7f62e149277ecc6289'
-var url = `http://api.openweathermap.org/data/2.5/forecast?q=SanDiego,us&APPID=${key}`
+const key = '6f0a0d476e913d7f62e149277ecc6289';
+var url = `http://api.openweathermap.org/data/2.5/forecast?zip=92126,us&APPID=${key}`;
 
+//setting up for request modules for connecting the openweatherap
+var request = require('request');
 //setting up express
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-//setting up 
+app.use(express.static('public'));
+app.get('/',function(req, res){
+	request(url, function(err, response, body){		
+		var data = JSON.parse(body);
+		res.send(data);
+
+	});
+})
+
+app.listen(3000, function(){
+
+})
