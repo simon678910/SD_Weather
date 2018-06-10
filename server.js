4dae00175330f6bdd04f516329cd24587f4e1a08
simon <simon@localhost.localdomain>
2018-06-09 22:21:45 -0700

    reponse a json file with 5 day data

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //setting up for the openweathermap
 const key = '6f0a0d476e913d7f62e149277ecc6289';
-var url = `http://api.openweathermap.org/data/2.5/forecast?zip=92126,us&APPID=${key}`;
+var url = `http://api.openweathermap.org/data/2.5/forecast?zip=92126,us&units=imperial&APPID=${key}`;
 
 //setting up for request modules for connecting the openweatherap
 var request = require('request');
@@ -10,15 +10,38 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/',function(req, res){
+	/* Access the Openweathermap website */
 	request(url, function(err, response, body){		
+		/* Get the data into a json file */
 		var data = JSON.parse(body);
-//		for(i = 0; i < data.list.length; i++){
-//			console.log(data.list[i]);
-//		}
-		res.send(data.city.name);
+		res.send(createJson(data));
 	});
 })
 
+function fiveDayData(min, max, humidity, description, icon){
+	this.min = min;
+	this.max = max;
+	this.description = description;
+	this.humidity = humidity;
+	this.icon = icon;
+}
+
+function createJson(data){
+	var min = [];
+	var max = [];
+	var humidity = [];
+	var description = [];
+	var icon = [];
+	for(i = 0; i < data.list.length/8; i++){
+		min.push(data.list[i*8].main.temp_min);
+		max.push(data.list[i*8].main.temp_max);
+		humidity.push(data.list[i*8].main.humidity);
+		description.push(data.list[i*8].weather[0].description);
+		icon.push(data.list[i*8].weather[0].icon);
+	}
+	var dataJson = JSON.stringify(new fiveDayData(min, max, humidity, description, icon));
+	return dataJson;
+}
 app.listen(3000, function(){
 
 })
