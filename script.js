7e0bdbf350d3f0b51f53384238d799ca3be2f4ab
simon <simon@localhost.localdomain>
2018-06-09 23:06:23 -0700

    Update the forcast table from JS

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+/* server's url*/
+const url = "http://localhost:3000";
+
+/* Row number */
+const rMax = 2
+const rMin = 3
+const rHumidity = 4
+
+
 $(document).ready(function(){
 
-	initDates();
+	$.get(url, function(data, status){
+		var weatherData = JSON.parse(data);
+		initDates();
+		setMax(weatherData);
+		setMin(weatherData);
+		setHumidity(weatherData);
 
-	//$("#today").text(constructDate(0));
-
+	})
+	.fail(function(request, status, error){
+		alert(status);
+	});
 });
 
 /* Helper function for getting the dates and construct to the mm/dd/yyyy structure
@@ -14,17 +30,35 @@ $(document).ready(function(){
 function constructDate(days){
 	var date = new Date();
 	date.setDate(date.getDate() + days);
-	console.log(date.getDate());
 	return (date.getMonth()+1)+'/'+(date.getDate())+'/'+date.getFullYear();
 }
 
-/* Initialize the today's date and the following 4days */
+/* Initialize the today's date and the following 5days */
 function initDates(){
-	/*Have 5 days*/
-	for(i = 0; i < 5;i++){
+	/*Total have a 6 days*/
+	for(i = 0; i < 6;i++){
 		if(i == 0)
 			$("#today").text(constructDate(i));
 		else
-			$("#future tr:first-child th:nth-child("+ (i) + ")").text(constructDate(i));
+			$("#future tr:first-child th:nth-child("+ i + ")").text(constructDate(i));
+	}
+}
+function setMax(data){
+	for(i = 0; i < 5; i++){
+		$("#future tr:nth-child(" + rMax + ") th:nth-child(" + (i+1) + ")")
+		.text(data.max[i]);
+	}
+}
+
+function setMin(data){
+	for(i = 0; i < 5; i++){
+		$("#future tr:nth-child(" + rMin + ") th:nth-child(" + (i+1) + ")")
+		.text(data.min[i]);
+	}
+}
+function setHumidity(data){
+	for(i = 0; i < 5; i++){
+		$("#future tr:nth-child(" + rHumidity + ") th:nth-child(" + (i+1) + ")")
+		.text(data.humidity[i]);
 	}
 }
